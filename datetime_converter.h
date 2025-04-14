e405e2f0c97eb489fb55c5bb174cd28e4d65d626
Morteza Maghrebi <maghrebimorteza@gmail.com>
2025-04-14 14:18:04 +0330

    Create datetime_converter.h

---
+++ datetime_converter.h
@@ -0,0 +1,38 @@
+#ifndef DATETIME_CONVERTER_H
+#define DATETIME_CONVERTER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define DAY_SECONDS 86400
+
+typedef struct 
+{
+	uint32_t year;
+	uint32_t month;
+	uint32_t day;
+	uint32_t hour;
+	uint32_t minute;
+	uint32_t second;
+	uint32_t weekday;
+} DateTimeGregorian;
+
+typedef struct 
+{
+	uint32_t year;
+	uint32_t month;
+	uint32_t day;
+	uint32_t hour;
+	uint32_t minute;
+	uint32_t second;
+	uint32_t weekday;
+} DateTimeSolar;
+
+bool isleap(int year);
+bool iskabise(int year);
+uint32_t Convert_DateTimeGregorian_Int32(DateTimeGregorian date);
+DateTimeGregorian Convert_Int32_DateTimeGregorian(uint32_t counter);
+uint32_t Convert_DateTimeSolar_Int32(DateTimeSolar date);
+DateTimeSolar Convert_Int32_DateTimeSolar(uint32_t counter);
+
+#endif // DATETIME_CONVERTER_H
